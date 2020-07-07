4249fa9878f48f316d90d9c8f3791a3fd32283ad
parahaoer <884101054@qq.com>
2020-07-07 11:35:29 +0800

    路径总和

---
+++ src/Tree/hasPathSumWithInorder.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        if(root == null) return false;
+        Stack<Pair<TreeNode, Integer>> s = new Stack();
+
+        TreeNode cur = root;
+
+        while(!s.isEmpty() || cur != null) {
+            while(cur != null) {
+                sum = sum - cur.val;
+                
+                s.push(new Pair(cur, sum));
+                cur = cur.left;
+            }
+            Pair<TreeNode, Integer> pair  = s.pop();
+
+            TreeNode node = pair.getKey();
+            sum = pair.getValue();
+            
+            if(node.left == null && node.right == null && sum == 0) return true;
+
+            if(node.right != null) 
+                cur = node.right;
+        }
+        return false;
+        
+    }
+}
